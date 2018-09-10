a64079643315dd050f7102a4dc5fe43d506ede1d
Mark Alston <malston@pivotal.io>
2018-09-10 07:00:04 -0600

    Configure deploy against vsphere homelab

--- deploy-concourse.sh
+++ deploy-concourse.sh
@@ -17,21 +17,22 @@ bosh deploy -d concourse $CONCOURSE_BOSH_DEPLOYMENT/cluster/concourse.yml \
    -v deployment_name=concourse \
    -v network_name=default \
    -v web_network_name=default \
-   -v web_network_vm_extension=lb \
    -v external_host=${CONCOURSE_HOST} \
    -v external_url="https://${CONCOURSE_HOST}" \
    -v web_vm_type=large \
    -v db_vm_type=large \
-   -v worker_vm_type=extra-large \
+   -v worker_vm_type=2xlarge.disk \
    -v db_persistent_disk_type=100GB \
    -o $CONCOURSE_BOSH_DEPLOYMENT/cluster/operations/basic-auth.yml \
    -o $CONCOURSE_BOSH_DEPLOYMENT/cluster/operations/privileged-http.yml \
    -o $CONCOURSE_BOSH_DEPLOYMENT/cluster/operations/privileged-https.yml \
    -o $CONCOURSE_BOSH_DEPLOYMENT/cluster/operations/tls.yml \
    -o $CONCOURSE_BOSH_DEPLOYMENT/cluster/operations/tls-vars.yml \
-   -o $CONCOURSE_BOSH_DEPLOYMENT/cluster/operations/web-network-extension.yml \
    -o operations/add-credhub-uaa-to-web.yml \
    -o operations/enable-db-backups.yml \
    -o operations/backup-atc-db.yml \
    -o operations/backup-uaa-db.yml \
    -o operations/backup-credhub-db.yml
+
+   # -o $CONCOURSE_BOSH_DEPLOYMENT/cluster/operations/web-network-extension.yml \
+   # -v web_network_vm_extension=lb \
